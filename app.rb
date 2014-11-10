8c79192ca37e3561a2a528e271ba0179b5fe6e9a
myzen2 <mehdi.zenbil@orange.fr>
2014-11-10 15:21:56 +0100

    simplification du code

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'rubygems'
 require 'dm-core'
 require 'dm-migrations'
 
@@ -8,52 +7,39 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/developm
 
 class Task
   include DataMapper::Resource
-
   property :id, Serial
   property :name, String
+  property :User, String
   property :completed_at, DateTime
-end
 
+ def completed?
+    true if completed_at
+  end
 
-get '/' do
-  "I did it my way !"
-end
+  def self.completed
+    all(:completed_at.not => nil)
+  end
+  def link
+    "<a href=\"task/#{self.id}\">#{self.name}</a>"
+  end
 
-get '/about' do
-  erb :about
-end
-
-# Saisir une nouvelle tâche
-get '/task/new' do
-  erb :new
 end
 
 # Créer une nouvelle tâche
 post '/task/create' do
   task = Task.new(:name => params[:name])
+  user = Task.new(:name => params[:User])
   if task.save
     status 201
-    redirect '/task/' + task.id.to_s
+    redirect '/'
   else
     status 412
-    redirect '/tasks'
+    redirect '/'
   end
 end
 
-# Afficher une tâche
-get '/task/:id' do
-  @task = Task.get(params[:id])
-  erb :task
-end
-
-# Afficher toutes les tâches
-get '/index' do
-  @tasks = Task.all
-  erb :index
-end
-
 # Modifier une tâche existante
-get '/task/:id/edit' do
+get '/task/:id' do
   @task = Task.get(params[:id])
   erb :edit
 end
@@ -65,10 +51,10 @@ put '/task/:id' do
   task.name = (params[:name])
   if task.save
     status 201
-    redirect '/task/' + task.id.to_s
+    redirect '/'
   else
     status 412
-    redirect '/tasks'
+    redirect '/'
   end
 end
 
@@ -81,7 +67,13 @@ end
 # Supprimer une tâche
 delete '/task/:id' do
   Task.get(params[:id]).destroy
-  redirect '/index'  
+  redirect '/'  
+end
+
+# Afficher toutes les tâches
+get '/' do
+  @tasks = Task.all
+  erb :index
 end
 
 DataMapper.auto_upgrade!
